d30d1c039bcd609c8e9439ec803a0cd916e858bb
Dennis Gorman <de465952@ucf.edu>
2024-09-04 14:11:18 -0400

    start up files.

---
+++ Projects/Backend/main.js
@@ -0,0 +1,34 @@
+const urlBase = 'http://cm.matthewe.me';
+const extension = 'php';
+
+function register() {
+
+}
+
+function login() {
+
+}
+
+function logout() {
+
+}
+
+function getAllContacts() {
+
+}
+
+function addContact() {
+
+}
+
+function deleteContact() {
+
+}
+
+function updateContact() {
+
+}
+
+function findContact() {
+
+}
