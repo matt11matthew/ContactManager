ab28b21b18608e8af69c8ee05b5b862878c463d4
matt11matthew <matt11matthew@gmail.com>
2024-09-10 09:36:54 -0400

    Update login.php

--- Projects/Backend/login.php
+++ Projects/Backend/login.php
@@ -6,19 +6,20 @@
     // Get json data from frontend
     $inData = getRequestInfo();
 
-    // Variables for user information
-    $userId = 0;
-    $username = '';
-    $password = '';
 
-    $query = "SELECT ID, FirstName, LastName FROM Logins WHERE UserName=? AND Password=?";
-    $stmt = $conn->prepare();
-    $stmt->bind_param('ss', $inData['username'], $inData['password']); // might need name changes based on front end HTML
+
+    $query = "SELECT * FROM Logins WHERE UserName=?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('ss', $inData['username']); // might need name changes based on front end HTML
     $stmt->execute();
     $result = $stmt->get_result();
 
+
     // Does the username/password pair exist?
     if ($row = $result->fetch_assoc()) {
+        if ($inData['password']!==$row['Password']) {
+            returnWithError('Invalid password');
+        }
         returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
     } else returnWithError('Account not found.');
 
