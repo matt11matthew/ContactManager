59cda286f3afdb56f78d1e7a462a7b614d60787c
j21j <156137501+j21j@users.noreply.github.com>
2024-09-11 13:25:41 -0400

    Add files via upload
    Updated files for login page. -Will

---
+++ Projects/Frontend/testLogin.js
@@ -0,0 +1,57 @@
+// const url = 'http://name.com/whatever';
+
+let userId = 0;
+let fname = "";
+let lname = "";
+
+function UserLogin(){
+    userId = 0;
+    fname = "";
+    lname = "";
+
+
+    //get information from the html inputs.
+    let userLogin = document.getElementById("username").value;
+    let userPass = document.getElementById("password").value;
+
+    //turn info into json formatting.
+    let tmp = {username: userLogin, password: userPass};
+    let LoginJson = JSON.stringify(tmp);
+
+    //url location of php file.
+    let url ="http://cm.matthewe.me/testing/Backend/login.php";
+
+    //post req to the rest api:
+    let xml = new XMLHttpRequest();
+
+    //temporarily using static url:
+    xml.open("POST", url, true);
+    xml.send(LoginJson);
+    //when a response is ready.
+    xml.onreadystatechange = function (){
+        //200: Successfully logged in. - via swaggerhub.
+        if(xml.readyState===4 && xml.status === 200){
+            let response;
+            //get the response.
+            //try to see if it is a json value, if not then it is a string and there was a login issue.
+            try{
+                response = JSON.parse(xml.responseText);
+            }catch(error){
+                response = null;
+                //output error msg:
+                document.getElementById("loginError").innerHTML = "There was an issue with username or password.";
+            }
+            //if we got a json file
+            if(response != null){
+                //save the information we got about user:
+                userId = response.id;
+                fname = response.firstName;
+                lname = response.lastName;
+
+                //direct user to the menu:
+                window.open("testMenu.html");
+            }
+        }
+    }
+
+}
