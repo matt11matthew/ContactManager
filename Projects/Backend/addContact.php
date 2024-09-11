a7a98633faa4c84c7fe1f93e5f9c585d03e67dca
Dennis Gorman <de465952@ucf.edu>
2024-09-11 16:11:26 -0400

    addContact.php should be mostly done

--- Projects/Backend/addContact.php
+++ Projects/Backend/addContact.php
@@ -3,17 +3,26 @@
     // Include config file for database connection
     include 'config.php';
 
-    # Get json data from frontend
+    // Get json data from frontend
     $inData = getRequestInfo();
 
+    // Variables to store userId and contact information
     $userId = $inData["userId"];
     $firstName = $inData["first"];
     $lastName = $inData["last"];
     $email = $inData["email"];
 
-    $stmt = $conm->prepare("INSERT INTO Contacts (UserID, FirstName, LastName, UserName, Email) VALUES ('$id', '$firstName', '$lastName', /*'$userName',*/ '$email')");
-    $stmt->bind_param("ss",$userId,$color);
+    // Prepare and execute SQL query to add contact
+    $query = "INSERT INTO Contacts (UserID, FirstName, LastName, Email) VALUES ('$userId', '$firstName', '$lastName', '$email')";
+    $stmt = $conm->prepare($query);
     $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result != 0) {
+        returnWithInfo($firstName, $lastName, $userId);
+    } else {
+        returnWithError("Contact not added");
+    }
 
     // Close connection
     $stmt->close();
